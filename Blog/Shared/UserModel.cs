a00067d280d9c99ce8879c0e654163af5696b141
Albert <ImmediateRent@outlook.com>
2022-04-03 17:39:09 -0600

    Added UserModel class

---
+++ Blog/Shared/UserModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Shared
+{
+    [Table("user_table")]
+    public class UserModel
+    {
+        [Column("user_id")]
+        [Key]
+        public int UserId { get; set; }
+
+        [Column("username")]
+        public string Username { get; set; }
+
+        [Column("first_name")]
+        public string FirstName { get; set; }
+
+        [Column("last_name")]
+        public string LastName { get; set; }
+
+        [Column("email")]
+        public string Email { get; set; }
+
+        [Column("password")]
+        public string Password { get; set; }
+    }
+}
