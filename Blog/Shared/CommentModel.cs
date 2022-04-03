e6aec6142a3edbf8dd4943796960503e317de95b
Albert <ImmediateRent@outlook.com>
2022-04-03 15:52:50 -0600

    Added connection to APIs and the BlogList page

---
+++ Blog/Shared/CommentModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Blog.Shared
+{
+    public class CommentModel
+    {
+        [JsonPropertyName("_id")]
+        public string? CommentId { get; set; }
+
+        [JsonPropertyName("post_id")]
+        public string? PostId { get; set; }
+
+        [Required(ErrorMessage = "Require Message")]
+        [JsonPropertyName("comment_message")]
+        public string? CommentMessage { get; set; }
+    }
+}
---
+++ Blog/Shared/PostModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Blog.Shared
+{
+    public class PostModel
+    {
+        [JsonPropertyName("_id")]
+        public string? PostId { get; set; }
+
+        [Required(ErrorMessage = "Require Title")]
+        [JsonPropertyName("title")]
+        public string? PostTitle { get; set; }
+
+        [Required(ErrorMessage = "Require Content")]
+        [JsonPropertyName("post_message")]
+        public string? PostContent { get; set; }
+    }
+}
